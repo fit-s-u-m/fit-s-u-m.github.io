e64cb860712fdfd5292d9e819ada01941b6c5988
fit-s-u-m <nehemiahmaths@gmail.com>
2024-10-28 03:13:41 +0300

    added plugin for rss feed

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,5 +1,5 @@
-import { QuartzConfig } from "./quartz/cfg"
-import * as Plugin from "./quartz/plugins"
+import { QuartzConfig } from "./quartz/cfg";
+import * as Plugin from "./quartz/plugins";
 
 /**
  * Quartz 4.0 Configuration
@@ -71,6 +71,7 @@ const config: QuartzConfig = {
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.ContentIndex(),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
@@ -88,6 +89,6 @@ const config: QuartzConfig = {
       Plugin.NotFoundPage(),
     ],
   },
-}
+};
 
-export default config
+export default config;
