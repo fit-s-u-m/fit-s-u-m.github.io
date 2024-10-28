99d9238b92844a29a37a24f14482bfbd8039aa93
fit-s-u-m <nehemiahmaths@gmail.com>
2024-10-28 23:57:12 +0300

    added the baseUrl for rss feed

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "fit-s-u-m.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
