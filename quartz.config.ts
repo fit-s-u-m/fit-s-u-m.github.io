6b3e5ffc763aea9b2575d4c1f209289a6a35e078
fit-s-u-m <nehemiahmaths@gmail.com>
2024-11-03 12:08:35 +0300

    made git to be main source for published date

--- quartz.config.ts
+++ quartz.config.ts
@@ -56,7 +56,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "filesystem"],
+        priority: ["git", "filesystem"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
